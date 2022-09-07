5997b33a44c21e19ecb63a80b574a61c0db00811
Saara Leppis <saara@leppihalme.fi>
2022-09-07 15:17:59 +0300

    styling ready for bigger screen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const btn = document.querySelector('.subscribe-tool')
-
+const navigation = document.querySelector('.main-menu');
+console.log(navigation)
 // display values on load
 const displayValues = document.querySelectorAll('.count');
 const speed = 50000;
@@ -20,4 +21,19 @@ displayValues.forEach((displayValue) => {
 
 btn.addEventListener('submit', () => {
   alert('Thank you to subscribe our news letter!')
+})
+
+// mobile menu 
+const hamburger = document.querySelector(".menu-icon")
+
+hamburger.addEventListener("click", () => {
+  navigation.classList.toggle('open')
+})
+
+navigation.addEventListener('click', () => {
+  if (document.body.clientWidth < 1000) {
+    // hamburger.classList.remove("close")
+    navigation.classList.add("mobile-menu")
+    console.log('now')
+  }
 })
